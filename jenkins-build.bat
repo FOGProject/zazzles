680ef018d32f40304a04f827df5e927a52814828
Jbob <contact@jbob.io>
2016-01-04 16:23:41 -0600

    Remote targets file on run

--- jenkins-build.bat
+++ jenkins-build.bat
@@ -1,5 +1,7 @@
 @Echo off
-echo "Running Windows Build Script"
+echo Running Windows Build Script
+del .\.nuget\NuGet.targets
+
 msbuild .\Zazzles.sln /p:PlatformTarget=x86 /p:Configuration=Release
 .\.nuget\NuGet.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory .\testrunner
 .\testrunner\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe .\bin\Zazzles.Tests.dll /xml=nunit-result.xml
