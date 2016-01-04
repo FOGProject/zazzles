55f80605c0dadc23071ebda174cfe0f0842824a0
Jbob <contact@jbob.io>
2016-01-04 13:01:39 -0600

    Add jenkins build scripts

---
+++ jenkins-build.bat
@@ -0,0 +1,4 @@
+@Echo off
+msbuild .\Zazzles.sln /p:PlatformTarget=x86 /p:Configuration=Release
+.\.nuget\NuGet.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory .\testrunner
+.\testrunner\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe .\bin\Zazzles.Tests.dll /xml=nunit-result.xml
