4d6b276486971df6c311a99806bf6c54f0496fab
Joe Schmitt <jbob182@gmail.com>
2016-01-05 14:59:44 -0600

    Update jenkins-build.bat

--- jenkins-build.bat
+++ jenkins-build.bat
@@ -1,7 +1,11 @@
 @Echo off
+Echo(
+Echo(
+echo ============================
 echo Running Windows Build Script
-del .\.nuget\NuGet.targets
+echo ============================
 
-msbuild .\Zazzles.sln /p:PlatformTarget=x86 /p:Configuration=Release
+.\.nuget\NuGet.exe restore .\
+msbuild .\Zazzles.sln /p:Platform="Any CPU" /p:Configuration=Release
 .\.nuget\NuGet.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory .\testrunner
 .\testrunner\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe .\bin\Zazzles.Tests.dll /xml=nunit-result.xml
