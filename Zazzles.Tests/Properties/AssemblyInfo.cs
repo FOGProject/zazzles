bdd9110381862ee7829268cd12f11d0807e44ff7
Jbob <contact@jbob.io>
2016-01-07 23:26:01 -0600

    Increment assembly info

--- Zazzles.Tests/Properties/AssemblyInfo.cs
+++ Zazzles.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Zazzles.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("FOG")]
 [assembly: AssemblyProduct("Zazzles.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
